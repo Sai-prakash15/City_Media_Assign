7e686a2b523fab7acc4201e23de84905e8455f47
Sai Prakash <ch.saiprakash22@gmail.com>
2021-06-10 19:39:39 +0530

    Define models

---
+++ movie/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+import datetime
+
+def current_year():
+    return datetime.date.today().year
+
+def upload_movie_image(instance, filename):
+    return "movies/{filename}".format(filename=filename)
+
+def upload_genre_image(instance, filename):
+    return "genre/{filename}".format(filename=filename)
+
+# Create your models here.
+
+class Genre(models.Model):
+    # Genre_CHOICES = (
+    #     ('AC', 'Action'),
+    #     ('DR', 'Drama'),
+    #     ('BI', 'Biography'),
+    #     ('CO', 'Comedy'),
+    #     ('AN', 'Animation'),
+    #     ('MS',' Mystery'),
+    #     ('AD', 'Adventure'),
+    #     ('CR', 'Crime')
+    # )
+
+    name = models.CharField(max_length=10,help_text='Enter Genre',unique=True)
+    image = models.ImageField(upload_to=upload_genre_image, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.name)[:]
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=10,help_text='Enter movie name',unique=True)
+    release_year = models.IntegerField(default=current_year)
+    genre = models.OneToOneField(Genre,on_delete=models.CASCADE)
+    rating = models.FloatField()
+    image = models.ImageField(upload_to=upload_movie_image, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.name)[:]
+
