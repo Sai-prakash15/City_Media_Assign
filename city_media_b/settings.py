e9f11d526658e9098dce17eba0623f5c89ac5ac0
Sai Prakash <ch.saiprakash22@gmail.com>
2021-06-12 20:52:26 +0530

    Add search filter to movie API on genre

--- city_media_b/settings.py
+++ city_media_b/settings.py
@@ -122,3 +122,14 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework.filters.SearchFilter',
+    ),
+    'SEARCH_PARAM': 'q',
+    'ORDERING_PARAM': 'ordering',
+}
